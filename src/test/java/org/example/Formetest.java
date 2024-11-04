bb91e97d6905bcab657ba36506fc76146dd9059e
PC <alexandrab2142@gmail.com>
2024-11-04 12:03:49 +0200

    PRIMUL COMMIT

---
+++ src/test/java/org/example/Formetest.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class FormeTest {
+
+    public static WebDriver driver; public Void BeforeClass;
+        {    System.setProperty("webdriver.chrome.driver", "C:\\chromedriver-win64\\chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.get("http://google.com");
+        driver.quit();
+    }
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver-win64\\chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.get("http://google.com");
+        driver.quit();}
+}
